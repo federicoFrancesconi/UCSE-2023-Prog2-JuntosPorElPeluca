1c646e42632632f53d1e6f7578b28ede8c1b2d35
MateoTomas0912 <99299338+MateoTomas0912@users.noreply.github.com>
2023-10-10 17:59:45 -0300

    repository envio y camion

--- repositories/CamionRepository.go
+++ repositories/CamionRepository.go
@@ -1,1 +1,23 @@
 package repositories
+
+import (
+	"UCSE-2023-Prog2-TPIntegrador/database"
+	"UCSE-2023-Prog2-TPIntegrador/model"
+)
+
+type CamionRepositoryInterface interface {
+	CrearCamion(camion *model.Camion) error
+	ObtenerCamionPorPatente(patente string) (*model.Camion, error)
+	ObtenerCamiones() ([]*model.Camion, error)
+	ActualizarCamion(camion *model.Camion) error
+}
+
+type CamionRepository struct {
+	db database.DB
+}
+
+func NewCamionRepository(db database.DB) *CamionRepository {
+	return &CamionRepository{
+		db: db,
+	}
+}
--- repositories/EnvioRepository.go
+++ repositories/EnvioRepository.go
@@ -1,1 +1,23 @@
 package repositories
+
+import (
+	"UCSE-2023-Prog2-TPIntegrador/database"
+	"UCSE-2023-Prog2-TPIntegrador/model/envios"
+)
+
+type EnvioRepositoryInterface interface {
+	CrearEnvio(envio *envios.Envio) error
+	ObtenerEnvioPorId(id int) (*envios.Envio, error)
+	ObtenerEnvios() ([]*envios.Envio, error)
+	ActualizarEnvio(envio *envios.Envio) error
+}
+
+type EnvioRepository struct {
+	db database.DB
+}
+
+func NewEnvioRepository(db database.DB) *EnvioRepository {
+	return &EnvioRepository{
+		db: db,
+	}
+}
