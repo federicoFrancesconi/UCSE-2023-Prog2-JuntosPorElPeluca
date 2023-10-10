9f7313b776701378ec4b036a6840137a1e3c8fe6
Federico Francesconi <108153799+federicoFrancesconi@users.noreply.github.com>
2023-10-10 12:24:11 -0300

    ProductoRepository base

--- repositories/ProductoRepository.go
+++ repositories/ProductoRepository.go
@@ -1,1 +1,24 @@
 package repositories
+
+import (
+	"UCSE-2023-Prog2-TPIntegrador/database"
+	"UCSE-2023-Prog2-TPIntegrador/model/productos"
+)
+
+type ProductoRepositoryInterface interface {
+	CrearProducto(producto *productos.Producto) error
+	ObtenerProductoPorId(id int) (*productos.Producto, error)
+	ObtenerProductos() ([]*productos.Producto, error)
+	ActualizarProducto(producto *productos.Producto) error
+	EliminarProducto(id int) error
+}
+
+type ProductoRepository struct {
+	db database.DB
+}
+
+func NewProductoRepository(db database.DB) *ProductoRepository {
+	return &ProductoRepository{
+		db: db,
+	}
+}
