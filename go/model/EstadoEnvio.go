7c2cd3ed6f35cacf1c30af5518d38b92585c719d
Federico Francesconi <108153799+federicoFrancesconi@users.noreply.github.com>
2023-11-02 11:30:56 -0300

    Validacion de estado de pedido

--- go/model/EstadoEnvio.go
+++ go/model/EstadoEnvio.go
@@ -4,10 +4,10 @@ type EstadoEnvio string
 
 const (
 	ADespachar EstadoEnvio = "A Despachar"
-	EnRuta    EstadoEnvio = "En Ruta"
+	EnRuta     EstadoEnvio = "En Ruta"
 	Despachado EstadoEnvio = "Despachado"
 )
 
-func EsUnEstadoEnvioValido(estado string) bool {
-	return estado == string(ADespachar) || estado == string(EnRuta) || estado == string(Despachado)
+func EsUnEstadoEnvioValido(estado EstadoEnvio) bool {
+	return estado == ADespachar || estado == EnRuta || estado == Despachado
 }
