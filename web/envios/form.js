c65c761b8f7fb0a8bb53330bdb715e36265ec6ce
MateoTomas0912 <99299338+MateoTomas0912@users.noreply.github.com>
2023-11-01 21:19:35 -0300

    tabla de pedidos en envio

--- web/envios/form.js
+++ web/envios/form.js
@@ -14,22 +14,88 @@ document.addEventListener("DOMContentLoaded", function (event) {
       .addEventListener("submit", function (event) {
         guardarEnvio(event);
       });
+
+    obtenerPedidos();
   }
 });
+
+//obtiene los productos para mostrar en el form de crear
+function obtenerPedidos() {
+  const urlConFiltro = `http://localhost:8080/pedidos`;
+
+  makeRequest(
+    `${urlConFiltro}`,
+    Method.GET,
+    null,
+    ContentType.JSON,
+    CallType.PRIVATE,
+    exitoObtenerPedidos,
+    errorPedido
+  );
+}
+
+function exitoObtenerPedidos(data) {
+  const elementosTable = document //tabla en la que se colocan los envios que se obtienen
+    .getElementById("tableProductos")
+    .querySelector("tbody");
+
+  data.forEach((elemento) => {
+    const row = document.createElement("tr"); //crear una fila
+
+    row.innerHTML = ` 
+                  <td><input type="checkbox" class="producto-checkbox"></td>
+                  <td>${elemento.codigo_producto}</td>
+                  <td>${elemento.nombre}</td>
+                  <td><input type="text" placeholder="Ingrese la cantidad"></td>
+                  <td>${elemento.precio_unitario}</td>
+                  <td>${elemento.peso_unitario}</td>
+                 `;
+
+    elementosTable.appendChild(row);
+  });
+}
+
+function obtenerPedidosArray() {
+  var PedidosSeleccionados = [];
+  let checkboxes = document.querySelectorAll(".pedido-checkbox");
+  checkboxes.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      // Agregar el producto seleccionado al objeto ProductosSeleccionados
+      var tr = checkbox.closest("tr");
+
+      var idPedido = tr.cells[1].textContent;
+      var ciudadDestino = tr.cells[2].textContent;
+      var estado = tr.cells[3].textContent;
+      var fechaCreacion = tr.cells[4].textContent;
+      var fechaUltimaActualizacion = tr.cells[5].textContent;
+      var idCreador = tr.cells[6].textContent;
+      var idPedido = tr.cells[7].textContent;
+
+      var pedidoSeleccionado = {
+        id: idPedido,
+        fecha_creacion: fechaCreacion,
+        fecha_ultima_actualizacion: fechaUltimaActualizacion,
+        ciudad_destino: ciudadDestino,
+        productos_elegidos: [],
+        id_creador: idCreador,
+        estado: estado,
+      };
+
+      PedidosSeleccionados.push(pedidoSeleccionado);
+    }
+  });
+
+  return PedidosSeleccionados;
+}
 
 const urlConFiltro = `http://localhost:8080/envios`;
 
 function guardarEnvio() {
-  //obtengo los datos de los pedidos
-  const valorPedidos = document.getElementById("Pedidos").value;
-  const valoresSeparados = valorPedidos.split(",");
-  const pedidosArray = valoresSeparados.map(function (valor) {
-    return parseInt(valor);
-  });
+  const pedidosArray = obtenerPedidosArray();
 
   //armo la data a enviar
   const data = {
-    id: 3,
+    id: "",
     fecha_creacion: "2023-10-14T12:00:00Z",
     fecha_ultima_actualizacion: "2023-10-14T12:00:00Z",
     patente_camion: document.getElementById("PatenteCamion").value,
