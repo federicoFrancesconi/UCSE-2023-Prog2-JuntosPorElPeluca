800d4a7b333ce0abb128d2757f415b4f74f187ab
Mateo Tomas <99299338+MateoTomas0912@users.noreply.github.com>
2023-12-11 19:03:08 -0300

    reportes beneficio entre fechas

--- web/scripts.js
+++ web/scripts.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   dibujarGraficoEnvios();
 });
 
-function obtenerBeneficioEntreFechasMes() {
+function obtenerBeneficioEntreFechas() {
   var fechaDesde = document.getElementById("FechaDesde").value;
   var fechaHasta = document.getElementById("FechaHasta").value;
 
@@ -31,118 +31,38 @@ function obtenerBeneficioEntreFechasMes() {
     null,
     ContentType.JSON,
     CallType.PRIVATE,
-    exitoObtenerBeneficioEntreFechasMes,
+    exitoObtenerBeneficioEntreFechas,
     errorGraficos
   );
 }
 
-function exitoObtenerBeneficioEntreFechasMes(data) {
-  var montoFechas = [];
+function exitoObtenerBeneficioEntreFechas(data) {
+  var montoFechasMes = [];
+  var montoFechasAnio = [];
   var meses = [];
-
-  if (data.length == 0) {
-    document.getElementById("mensajeSinBeneficio").innerHTML = "No hay beneficios cargados en esas fechas";
-  }
-
-  for (let i = 0; i < data.length; i++) {
-    const element = data[i];
-    montoFechas.push(element.Monto);
-    meses.push(element.Nombre);
-  }
-
-  const datos = {
-    labels: meses,
-    datasets: [
-      {
-        data: montoFechas, // Monto por mes
-        backgroundColor: [
-          "#FF5733",
-          "#FFC300",
-          "#33FF57",
-          "#339CFF",
-          "#FFA500",
-        ], // Colores para cada sector del gráfico
-      },
-    ],
-  };
-
-  // Configuración del gráfico de barras
-  var configuracionBarras = {
-    responsive: true,
-    scales: {
-        y: {
-            beginAtZero: true
-        }
-    }
-  };
-
-  // Obtener el contexto del lienzo de barras
-  var contextoBarras = document.getElementById('graficoBeneficioMes').getContext('2d');
-
-  // Crear el gráfico de barras
-  var miGraficoBarras = new Chart(contextoBarras, {
-      type: 'bar',
-      data: datos,
-      options: configuracionBarras
-  });
-
-  // Dibuja el gráfico de barras en el elemento canvas 
-  new Chart(contextoBarras, miGraficoBarras);
-}
-
-function obtenerBeneficioEntreFechasAnio() {
-  var fechaDesde = document.getElementById("FechaDesde").value;
-  var fechaHasta = document.getElementById("FechaHasta").value;
-
-  var urlConFiltro = `http://localhost:8080/envios/beneficioEntreFechas`;
-
-  //Si fechaDesde esta vacio, no se agrega al filtro
-  if (fechaDesde != "") {
-    urlConFiltro += `?fechaDesde=${fechaDesde}`;
-  }
-
-  //Si fechaHasta esta vacio, no se agrega al filtro
-  if (fechaHasta != "") {
-    if (fechaDesde != "") {
-      urlConFiltro += `&fechaHasta=${fechaHasta}`;
-    } else {
-      urlConFiltro += `?fechaHasta=${fechaHasta}`;
-    }
-  }
-
-  //urlConFiltro = `http://localhost:8080/envios/beneficioEntreFechas?fechaDesde=${fechaDesde}&fechaHasta=${fechaHasta}`;
-
-  makeRequest(
-    `${urlConFiltro}`,
-    Method.GET,
-    null,
-    ContentType.JSON,
-    CallType.PRIVATE,
-    exitoObtenerBeneficioEntreFechasAnio,
-    errorGraficos
-  );
-}
-
-
-function exitoObtenerBeneficioEntreFechasAnio(data) {
-  var montoFechas = [];
   var anios = [];
 
   if (data.length == 0) {
     document.getElementById("mensajeSinBeneficio").innerHTML = "No hay beneficios cargados en esas fechas";
   }
 
-  for (let i = 0; i < data.length; i++) {
-    const element = data[i];
-    montoFechas.push(element.Monto);
+  for (let i = 0; i < data.meses.length; i++) {
+    const element = data.meses[i];
+    montoFechasMes.push(element.Monto);
+    meses.push(element.Nombre);
+  }
+
+  for (let i = 0; i < data.años.length; i++) {
+    const element = data.años[i];
+    montoFechasAnio.push(element.Monto);
     anios.push(element.Nombre);
   }
 
-  const datos = {
-    labels: anios,
+  const datosMeses = {
+    labels: meses,
     datasets: [
       {
-        data: montoFechas, // Monto por mes
+        data: montoFechasMes, // Monto por mes
         backgroundColor: [
           "#FF5733",
           "#FFC300",
@@ -165,17 +85,46 @@ function exitoObtenerBeneficioEntreFechasAnio(data) {
   };
 
   // Obtener el contexto del lienzo de barras
-  var contextoBarras = document.getElementById('graficoBeneficioMes').getContext('2d');
+  var contextoBarrasMeses = document.getElementById('graficoBeneficioMes').getContext('2d');
 
   // Crear el gráfico de barras
-  var miGraficoBarras = new Chart(contextoBarras, {
+  var miGraficoBarrasMeses = new Chart(contextoBarrasMeses, {
       type: 'bar',
-      data: datos,
+      data: datosMeses,
       options: configuracionBarras
   });
 
   // Dibuja el gráfico de barras en el elemento canvas 
-  new Chart(contextoBarras, miGraficoBarras);
+  new Chart(contextoBarras, miGraficoBarrasMeses);
+
+  const datosAnios = {
+    labels: anios,
+    datasets: [
+      {
+        data: montoFechasAnio, // Monto por anio
+        backgroundColor: [
+          "#FF5733",
+          "#FFC300",
+          "#33FF57",
+          "#339CFF",
+          "#FFA500",
+        ], // Colores para cada sector del gráfico
+      },
+    ],
+  };
+
+  // Obtener el contexto del lienzo de barras
+  var contextoBarrasAnio = document.getElementById('graficoBeneficioAnio').getContext('2d');
+
+  // Crear el gráfico de barras
+  var miGraficoBarrasAnios = new Chart(contextoBarrasAnio, {
+      type: 'bar',
+      data: datosAnios,
+      options: configuracionBarras
+  });
+
+  // Dibuja el gráfico de barras en el elemento canvas 
+  new Chart(contextoBarras, miGraficoBarrasAnios);
 }
 
 function errorGraficos(status, body) {
