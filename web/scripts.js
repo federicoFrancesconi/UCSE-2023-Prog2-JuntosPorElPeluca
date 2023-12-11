7cdd6f48af4f9d9e3081cd3a0a3dfce38695bf19
Mateo Tomas <99299338+MateoTomas0912@users.noreply.github.com>
2023-12-11 16:24:30 -0300

    ajustes barras y docker

--- web/scripts.js
+++ web/scripts.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   dibujarGraficoEnvios();
 });
 
-function obtenerBeneficioEntreFechas() {
+function obtenerBeneficioEntreFechasMes() {
   var fechaDesde = document.getElementById("FechaDesde").value;
   var fechaHasta = document.getElementById("FechaHasta").value;
 
@@ -31,13 +31,151 @@ function obtenerBeneficioEntreFechas() {
     null,
     ContentType.JSON,
     CallType.PRIVATE,
-    exitoObtenerBeneficioEntreFechas,
+    exitoObtenerBeneficioEntreFechasMes,
     errorGraficos
   );
 }
 
-function exitoObtenerBeneficioEntreFechas(data) {
-  document.getElementById("beneficio").innerHTML = data.beneficio;
+function exitoObtenerBeneficioEntreFechasMes(data) {
+  var montoFechas = [];
+  var meses = [];
+
+  if (data.length == 0) {
+    document.getElementById("mensajeSinBeneficio").innerHTML = "No hay beneficios cargados en esas fechas";
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    montoFechas.push(element.Monto);
+    meses.push(element.Nombre);
+  }
+
+  const datos = {
+    labels: meses,
+    datasets: [
+      {
+        data: montoFechas, // Monto por mes
+        backgroundColor: [
+          "#FF5733",
+          "#FFC300",
+          "#33FF57",
+          "#339CFF",
+          "#FFA500",
+        ], // Colores para cada sector del gráfico
+      },
+    ],
+  };
+
+  // Configuración del gráfico de barras
+  var configuracionBarras = {
+    responsive: true,
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+  };
+
+  // Obtener el contexto del lienzo de barras
+  var contextoBarras = document.getElementById('graficoBeneficioMes').getContext('2d');
+
+  // Crear el gráfico de barras
+  var miGraficoBarras = new Chart(contextoBarras, {
+      type: 'bar',
+      data: datos,
+      options: configuracionBarras
+  });
+
+  // Dibuja el gráfico de barras en el elemento canvas 
+  new Chart(contextoBarras, miGraficoBarras);
+}
+
+function obtenerBeneficioEntreFechasAnio() {
+  var fechaDesde = document.getElementById("FechaDesde").value;
+  var fechaHasta = document.getElementById("FechaHasta").value;
+
+  var urlConFiltro = `http://localhost:8080/envios/beneficioEntreFechas`;
+
+  //Si fechaDesde esta vacio, no se agrega al filtro
+  if (fechaDesde != "") {
+    urlConFiltro += `?fechaDesde=${fechaDesde}`;
+  }
+
+  //Si fechaHasta esta vacio, no se agrega al filtro
+  if (fechaHasta != "") {
+    if (fechaDesde != "") {
+      urlConFiltro += `&fechaHasta=${fechaHasta}`;
+    } else {
+      urlConFiltro += `?fechaHasta=${fechaHasta}`;
+    }
+  }
+
+  //urlConFiltro = `http://localhost:8080/envios/beneficioEntreFechas?fechaDesde=${fechaDesde}&fechaHasta=${fechaHasta}`;
+
+  makeRequest(
+    `${urlConFiltro}`,
+    Method.GET,
+    null,
+    ContentType.JSON,
+    CallType.PRIVATE,
+    exitoObtenerBeneficioEntreFechasAnio,
+    errorGraficos
+  );
+}
+
+
+function exitoObtenerBeneficioEntreFechasAnio(data) {
+  var montoFechas = [];
+  var anios = [];
+
+  if (data.length == 0) {
+    document.getElementById("mensajeSinBeneficio").innerHTML = "No hay beneficios cargados en esas fechas";
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    montoFechas.push(element.Monto);
+    anios.push(element.Nombre);
+  }
+
+  const datos = {
+    labels: anios,
+    datasets: [
+      {
+        data: montoFechas, // Monto por mes
+        backgroundColor: [
+          "#FF5733",
+          "#FFC300",
+          "#33FF57",
+          "#339CFF",
+          "#FFA500",
+        ], // Colores para cada sector del gráfico
+      },
+    ],
+  };
+
+  // Configuración del gráfico de barras
+  var configuracionBarras = {
+    responsive: true,
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+  };
+
+  // Obtener el contexto del lienzo de barras
+  var contextoBarras = document.getElementById('graficoBeneficioMes').getContext('2d');
+
+  // Crear el gráfico de barras
+  var miGraficoBarras = new Chart(contextoBarras, {
+      type: 'bar',
+      data: datos,
+      options: configuracionBarras
+  });
+
+  // Dibuja el gráfico de barras en el elemento canvas 
+  new Chart(contextoBarras, miGraficoBarras);
 }
 
 function errorGraficos(status, body) {
@@ -96,9 +234,32 @@ function exitoObtenerGraficoPedidos(data) {
     data: datos,
   };
 
-  // Dibuja el gráfico en el elemento canvas con id "chart"
-  const ctx = document.getElementById("graficoPedidos").getContext("2d");
+  // Dibuja el gráfico en el elemento canvas 
+  const ctx = document.getElementById("graficoPedidosTorta").getContext("2d");
   new Chart(ctx, config);
+
+  // Configuración del gráfico de barras
+  var configuracionBarras = {
+    responsive: true,
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+  };
+
+  // Obtener el contexto del lienzo de barras
+  var contextoBarras = document.getElementById('graficoPedidosBarra').getContext('2d');
+
+  // Crear el gráfico de barras
+  var miGraficoBarras = new Chart(contextoBarras, {
+      type: 'bar',
+      data: datos,
+      options: configuracionBarras
+  });
+
+  // Dibuja el gráfico de barras en el elemento canvas 
+  new Chart(contextoBarras, miGraficoBarras);
 }
 
 function dibujarGraficoEnvios() {
@@ -146,13 +307,33 @@ function exitoObtenerGraficoEnvios(data) {
     ],
   };
 
-  // Configuración del gráfico
+  // Configuración del gráfico de torta
   const config = {
     type: "pie",
     data: datos,
   };
 
-  // Dibuja el gráfico en el elemento canvas con id "chart"
-  const ctx = document.getElementById("graficoEnvios").getContext("2d");
+  // Dibuja el gráfico de torta en el elemento canvas 
+  const ctx = document.getElementById("graficoEnviosTorta").getContext("2d");
   new Chart(ctx, config);
+
+  // Configuración del gráfico de barras
+  var configuracionBarras = {
+    responsive: true,
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+  };
+
+  const configBarras = {
+    type: 'bar',
+      data: datos,
+      options: configuracionBarras
+  };
+
+  // Dibuja el gráfico de barras en el elemento canvas 
+  const ctxBarras = document.getElementById("graficoEnviosBarra").getContext("2d");
+  new Chart(ctxBarras, configBarras);
 }
